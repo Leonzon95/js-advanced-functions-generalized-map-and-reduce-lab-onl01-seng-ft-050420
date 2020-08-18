a7726c554db1d4791af5d4dd481b65dfe5a76d25
leobr <leobrionesherrera@gmail.com>
2020-08-17 22:01:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add your functions here
+function map (array, fn){
+    return array.map(fn);
+}
+
+function reduce(array, fn, start) {
+    if (!!start) return array.reduce(fn, start);
+    return  array.reduce(fn);
+}
